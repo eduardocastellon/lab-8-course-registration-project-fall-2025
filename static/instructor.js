00d8d221ecfd5324c3e44168c36713ba01d23dd7
Alex <132794767+Unknown903-dev@users.noreply.github.com>
2025-11-14 12:52:17 -0800

    Update instructor.js

--- static/instructor.js
+++ static/instructor.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", async () => {
+    //gets instructor id if no id go back to login
     const instructorId= localStorage.getItem("instructor_id");
     if(!instructorId){
         window.location.href="/";
         return;
     }
+
     const signout = document.querySelector(".signout");
     if(signout){
+        //when signout button is clicked clear the id and go back to login
         signout.addEventListener("click", (e) => {
-            e.preventDefault();
             localStorage.removeItem("instructor_id");
             window.location.href="/";
         });
     }
+
+    //grabs data
     const welcomeEl = document.getElementById("welcome");
     const tbody = document.getElementById("instructor-courses-body");
 
@@ -21,11 +25,14 @@ document.addEventListener("DOMContentLoaded", async () => {
             fetch("/courses")
         ]);
 
+        //convert http response to json
         const instructor = await instrRes.json();
         const allCourses = await coursesRes.json();
 
         welcomeEl.textContent=`welcome ${instructor.first_name}!`;
         const assignedIds = instructor.assigned_courses || [];
+        
+        //gets instructors courses  only
         const instructorCourses = allCourses.filter(c =>assignedIds.includes(c.unique_id));
 
         tbody.innerHTML="";
@@ -33,12 +40,15 @@ document.addEventListener("DOMContentLoaded", async () => {
         instructorCourses.forEach((course) => {
             const row = document.createElement("tr");
             row.style.cursor="pointer";
+
+            //fills the rows with the course info
             row.innerHTML=`
                 <td>${course.course_name}</td>
                 <td>${course.instructor}</td>
                 <td>${course.time}</td>
                 <td>${course.registered_students}/${course.capacity}</td>`;
             
+            //makes the rows clickable directs them to the grades for students
             row.addEventListener("click", () => {
                 window.location.href=`/instructor/course/${course.unique_id}`;
             });
